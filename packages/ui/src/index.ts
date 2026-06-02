e4cd5a0a21b8c6e2d4c875252f0feebd35d96ea6
Kacper Obrycki <kacperobrycki@gmail.com>
2026-06-02 22:53:13 +0200

    feat(ui): add index.ts barrel re-exporting all components

---
+++ packages/ui/src/index.ts
@@ -0,0 +1,94 @@
+export { Alert, AlertDescription, AlertTitle } from './components/alert.tsx'
+export { Badge, badgeVariants } from './components/badge.tsx'
+export {
+  Breadcrumb,
+  BreadcrumbEllipsis,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from './components/breadcrumb.tsx'
+export { Button, buttonVariants } from './components/button.tsx'
+export {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from './components/card.tsx'
+export { Checkbox } from './components/checkbox.tsx'
+export {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogOverlay,
+  DialogPortal,
+  DialogTitle,
+  DialogTrigger,
+} from './components/dialog.tsx'
+export {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from './components/dropdown-menu.tsx'
+export { Input } from './components/input.tsx'
+export { Label } from './components/label.tsx'
+export {
+  Popover,
+  PopoverAnchor,
+  PopoverContent,
+  PopoverTrigger,
+} from './components/popover.tsx'
+export {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectScrollDownButton,
+  SelectScrollUpButton,
+  SelectSeparator,
+  SelectTrigger,
+  SelectValue,
+} from './components/select.tsx'
+export { Separator } from './components/separator.tsx'
+export {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetOverlay,
+  SheetPortal,
+  SheetTitle,
+  SheetTrigger,
+} from './components/sheet.tsx'
+export { Skeleton } from './components/skeleton.tsx'
+export { Toaster } from './components/sonner.tsx'
+export { Tabs, TabsContent, TabsList, TabsTrigger } from './components/tabs.tsx'
+export { Textarea } from './components/textarea.tsx'
+export {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from './components/tooltip.tsx'
+export { cn } from './lib/utils.ts'
