afb13b24cbfd0696979ec461a9065d6ec24c3fdd
Kacper Obrycki <kacperobrycki@gmail.com>
2026-06-10 14:47:56 +0200

    fix(db): exclude integration tests from default test config

--- packages/db/rstest.config.ts
+++ packages/db/rstest.config.ts
@@ -4,4 +4,5 @@ export default defineConfig({
   globals: false,
   testEnvironment: 'node',
   include: ['src/test/**/*.{test,spec}.ts'],
+  exclude: ['src/test/**/*.integration.{test,spec}.ts'],
 })
